d46611e2f5e40094501c0ae941eeb685d74fc3dc
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-01-11 18:05:08 +0100

    place example script in example.js

--- dist/script.js
+++ dist/script.js
@@ -131,14 +131,3 @@ class NavbarAlternator {
   }
 }
 
-// ****************
-
-const navbarAlternator = new NavbarAlternator({
-  navbarSelector: "header",
-  targetSelector: "main > .hero",
-  elements: [
-    // elementSelector, [state 1 class, state 2 class]
-    ["header", ["navbar-animate-to-yellow", "navbar-animate-to-white"]],
-    ["header li.book-call button", ["button-navbar-animate-to-black", "button-navbar-animate-to-green"]],
-  ],
-});
