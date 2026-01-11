c9c3bbaf6227d89a7e27b88443b85335ed3343e8
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-01-11 16:06:06 +0100

    working version with clarity

--- dist/script.js
+++ dist/script.js
@@ -4,11 +4,13 @@ class NavbarAlternator {
     this.targetSelector = targetSelector;
     this.elements = elements;
     this.onTargetReached = onTargetReached;
-      
+
     this.lastScrollTimeout = null;
     this.scrollDelayMs = 100;
 
-    this.currentState = null
+    // null, 1, 2
+    // it's only temporary null, it will be immediately overriden
+    this.state = null;
 
     // if the document was loaded
     if (document.readyState == "complete") {
@@ -18,199 +20,166 @@ class NavbarAlternator {
     else {
       window.addEventListener("load", this.init.bind(this));
     }
-
   }
 
   init() {
-    this.addEventNavbarScroll();
     this.addProperNavbarAnimation();
+    this.addEventNavbarScroll();
   }
 
   // ******************
 
-
   handleScrollOnFinishDelay = () => {
-    // get the hero
     const hero = document.querySelector(this.targetSelector);
-    const heroCoordinates = hero.getBoundingClientRect();
-  
-    // console.log(heroCoordinates.bottom);
-  
-    // if scroll is before hero bottom
-  
-    /** this is HERO
-     * the number 100 represents the hero's bottom.
-     * 110 is an example to show a number > 100
-     *
-     * -------------------------------- DISTANCE: HERO BOTTOM TO VIEWPORT TOP: 100
-     *
-     * |                              |
-     * |                              |
-     * |                              |
-     * --------------------------------
-     *
-     */
-  
-    if (heroCoordinates.bottom >= this.getHeaderHeight()) {
-      // if no navbar animation exists, add the "to white" animation
-      if (this.existsToWhiteNavbarAnimation()) {
-        this.removeNavbarAnimationState2();
-        this.addNavbarAnimationState1();
+    const targetCoordinates = hero.getBoundingClientRect();
+
+    const targetReached = targetCoordinates.bottom >= this.getHeaderHeight();
+
+    // TARGET REACHED
+    if (targetReached) {
+      if (this.isState2()) {
+        this.removeState2();
+        this.addState1();
       }
-    } else {
-      // if no animation exists, add the "to white" animation
-      if (!this.existsAnyNavbarAnimation()) {
-        this.addNavbarAnimationState2();
-      }
-      // if the "to yellow" animation exists,
-      // remove it and add the "to white" animation
-      else if (this.existsToYellowNavbarAnimation()) {
-        this.removeNavbarAnimationState1();
-        this.addNavbarAnimationState2();
+    }
+    // TARGET NOT REACHED
+    else {
+      if (this.isState1()) {
+        this.removeState1();
+        this.addState2();
       }
     }
   };
-  
+
   // THE ENTIRE NAVBAR ANIMATION: STATE 1: ADD
-  addNavbarAnimationState1 = () => {
+  addState1 = () => {
     // navbar
     this.addToYellowNavbarAnimation();
     // button
-  //   addToBlackButtonNavbarAnimation();
+    //   addToBlackButtonNavbarAnimation();
   };
-  
+
   // THE ENTIRE NAVBAR ANIMATION: STATE 1: REMOVE
-  removeNavbarAnimationState1 = () => {
+  removeState1 = () => {
     // navbar
     this.removeToYellowNavbarAnimation();
     // button
-  //   removeToBlackButtonNavbarAnimation();
+    //   removeToBlackButtonNavbarAnimation();
   };
-  
+
   // THE ENTIRE NAVBAR ANIMATION: STATE 2: ADD
-  addNavbarAnimationState2 = () => {
+  addState2 = () => {
     // navbar
     this.addToWhiteNavbarAnimation();
     // button
-  //   addToGreenButtonNavbarAnimation();
+    //   addToGreenButtonNavbarAnimation();
   };
-  
+
   // THE ENTIRE NAVBAR ANIMATION: STATE 2: REMOVE
-  removeNavbarAnimationState2 = () => {
+  removeState2 = () => {
     // navbar
     this.removeToWhiteNavbarAnimation();
     // button
-  //   removeToGreenButtonNavbarAnimation();
+    //   removeToGreenButtonNavbarAnimation();
   };
-  
+
   // avoids triggering the scroll handler for every scroll
   // waits a small delay
   handleScroll = () => {
     clearTimeout(this.lastScrollTimeout);
     this.lastScrollTimeout = setTimeout(this.handleScrollOnFinishDelay.bind(this), this.scrollDelayMs);
   };
-  
-  
-  
+
   addEventNavbarScroll = () => {
     // get the hero y coordinate of bottom border
-  
+
     // if there's a scroll, and the y coordinate of the hero's
     // bottom border is less than the actual position of where the
     // viewport is at right now, activate the animation
-  
+
     window.addEventListener("scroll", this.handleScroll.bind(this));
   };
-  
-  
-  // executed only on page load, to immediately 
-  // apply the appropriate animation based 
+
+  // executed only on page load, to immediately
+  // apply the appropriate animation based
   // on navbar position/scroll
   addProperNavbarAnimation = () => {
-    const hero = document.querySelector(this.targetSelector);
-  
-    const heroCoordinates = hero.getBoundingClientRect();
-  
-    if (heroCoordinates.bottom < this.getHeaderHeight()) {
-      this.addNavbarAnimationState2()
+    const target = document.querySelector(this.targetSelector);
+
+    const targetCoordinates = target.getBoundingClientRect();
+
+    const isAfterTarget = targetCoordinates.bottom < this.getHeaderHeight();
+
+    if (isAfterTarget) {
+      this.setState(2);
+      this.addState2();
+    } else {
+      this.setState(1);
     }
   };
-  
-  
+
   getHeaderHeight = () => {
-    return 200
-  }
-  
+    return 200;
+  };
+
   // ADD ANIMATION
-  
+
   addToWhiteNavbarAnimation = () => {
     const navbar = document.querySelector(this.navbarSelector);
     navbar.classList.add("navbar-animate-to-white");
   };
-  
+
   addToYellowNavbarAnimation = () => {
     const navbar = document.querySelector(this.navbarSelector);
     navbar.classList.add("navbar-animate-to-yellow");
   };
-  
-  // const addToGreenButtonNavbarAnimation = () => {
-  //   const button = document.querySelector("header > .navbar button");
-  //   button.classList.add("button-navbar-animate-to-green");
-  // };
-  
-  // const addToBlackButtonNavbarAnimation = () => {
-  //   const button = document.querySelector("header > .navbar button");
-  //   button.classList.add("button-navbar-animate-to-black");
-  // };
-  
+
+
   // REMOVE ANIMATION
-  
+
   removeToWhiteNavbarAnimation = () => {
     const navbar = document.querySelector(this.navbarSelector);
     navbar.classList.remove("navbar-animate-to-white");
   };
-  
+
   removeToYellowNavbarAnimation = () => {
     const navbar = document.querySelector(this.navbarSelector);
     navbar.classList.remove("navbar-animate-to-yellow");
   };
-  
-  // const removeToGreenButtonNavbarAnimation = () => {
-  //   const navbar = document.querySelector("header > .navbar button");
-  //   navbar.classList.remove("button-navbar-animate-to-green");
-  // };
-  
-  // const removeToBlackButtonNavbarAnimation = () => {
-  //   const navbar = document.querySelector("header > .navbar button");
-  //   navbar.classList.remove("button-navbar-animate-to-black");
-  // };
-  
-  // EXISTS ANIMATION
-  
-  existsToWhiteNavbarAnimation = () => {
-    const navbar = document.querySelector(this.navbarSelector);
-    return navbar.classList.contains("navbar-animate-to-white");
-  };
-  
-  existsToYellowNavbarAnimation = () => {
-    const navbar = document.querySelector(this.navbarSelector);
-    return navbar.classList.contains("navbar-animate-to-yellow");
-  };
-  
-  existsAnyNavbarAnimation = () => {
-    return this.existsToWhiteNavbarAnimation() || this.existsToYellowNavbarAnimation();
-  };
 
 
+
+
+// 
+
+  setState(state) {
+    this.state = state;
+  }
+
+  setNextState() {
+    if (this.state == 1) {
+      this.state = 2;
+    } else if (this.state == 2) {
+      this.state = 1;
+    } else {
+      throw Error("current state not recognized");
+    }
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  isState1() {
+    return this.getState() == 1;
+  }
+
+  isState2() {
+    return this.getState() == 2;
+  }
 }
 
-
-
-
-
 // ****************
-
-
 
 new NavbarAlternator({
   navbarSelector: "header",
